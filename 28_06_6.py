03f85089a05318a869809c32af3ba6d7d2c43f00
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-07-02 18:40:43 +0400

    Add files via upload

---
+++ 28_06_6.py
@@ -0,0 +1,18 @@
+def map1(function, ls):
+    for i in ls:
+        print(function(i))
+
+def add_10(x):
+    if isinstance(x, (int, float)):
+        return x + 10
+    else: 
+        return ("You input is not int or float")
+
+ls1 = ['1', 5, 10]
+map1(add_10, ls1)
+
+
+'''You input is not int or float
+   15
+   20                             '''
+
