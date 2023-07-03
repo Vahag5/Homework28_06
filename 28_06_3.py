ef1174e864542958f511b50454f195c0979eae30
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-07-03 20:37:01 +0400

    Update 28_06_3.py

--- 28_06_3.py
+++ 28_06_3.py
@@ -3,11 +3,11 @@ def filtr(function, ls):
     for x in ls:
         if function(x):
             filtr_result.append(x)       
-    return filtr_result
+    yield filtr_result
 
 
 def not_even(n):
     return n % 2 != 0 
 
 ls1 = [4, 5, 6, 1, 7, 8, 9, 10] 
-print(filtr(not_even, ls1))
+print(list(filtr(not_even, ls1)))
