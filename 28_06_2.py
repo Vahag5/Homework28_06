13572369ab0494baa588891448f888d9bcbae575
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-07-06 10:28:46 +0400

    Add files via upload

---
+++ 28_06_2.py
@@ -0,0 +1,13 @@
+def zip1(*iterables):
+    iterators = list(map(iter, iterables))
+    while True:
+        try:
+            i = list(map(next, iterators))
+            yield tuple(i)
+        except StopIteration:
+            return
+
+tver = [1, 2, 3]
+tar = ['a', 'b', 'c']
+for i in zip(tar, tver):
+    print(i)
